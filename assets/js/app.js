1c2c104c6d782ff240704828672f9e8df4688b4f
Craig Melville <acekreations@gmail.com>
2018-08-09 23:28:58 -0700

    add addUserTag func and renderTitle func

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,23 +1,28 @@
 var defaultTags = ["Abstract", "Minimal", "Geometric", "Trippy", "Illustration", "3d", "Design", "Motion Graphics", "Pattern", "Color"];
 
 function renderTags(tagArr) {
+  $("#tagContainer").empty();
   for (var i = 0; i < tagArr.length; i++) {
-    var newBtn = $('<button type="button" class="btn btn-primary btn-sm mt-2 mr-1">');
+    var newBtn = $('<button type="button" class="btn btn-primary btn-sm mt-2 mr-1 tag">');
     newBtn.attr("data-tag", tagArr[i]);
     newBtn.text(tagArr[i]);
     $("#tagContainer").append(newBtn);
   }
 }
 
+function renderTitle(tag) {
+  $("#tagTitle").text(tag);
+}
+
 function fetchGifs(tag) {
-  //var tag = $(this).attr("data-tag");
+  renderTitle(tag);
   var apiKey = "nFQ89Mq5zf85yyf2d2OqQFzI7x9XfRWz";
   var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=" + apiKey + "&q=" + tag + "&limit=10&rating=g";
   $.ajax({
     url: queryURL,
     method: "GET"
   }).then(function(response){
-    console.log(response);
+    $("#gifContainer").empty();
     for (var i = 0; i < response.data.length; i++) {
       var stillUrl = response.data[i].images["480w_still"].url;
       var gifUrl = response.data[i].images.downsized.url;
@@ -27,11 +32,32 @@ function fetchGifs(tag) {
   })
 }
 
+function addUserTag() {
+  var userTag = $("#addTagInput").val().trim();
+  console.log(userTag);
+  if (userTag.length > 2 && defaultTags.indexOf(userTag) === -1) {
+    defaultTags.push(userTag);
+    renderTags(defaultTags);
+    fetchGifs(userTag);
+    $("#addTagInput").val("");
+  }
+}
+
 
 //jquery ready
 $(function(){
+//initialize
 renderTags(defaultTags);
+fetchGifs("Abstract");
 
-fetchGifs("design");
+$("#tagContainer").on("click", ".tag", function(){
+  var tag = $(this).attr("data-tag");
+  fetchGifs(tag);
+});
+
+$("#addTag").on("click", function(){
+  event.preventDefault();
+  addUserTag();
+});
 
 });
