0c629894b58e275a261f042d4e4bdd08bbf0ad9e
Craig Melville <acekreations@gmail.com>
2018-08-10 17:48:48 -0700

    styling update and add download button

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,9 +1,9 @@
-var defaultTags = ["Abstract", "Minimal", "Geometric", "Trippy", "Illustration", "3d", "Design", "Motion Graphics", "Pattern", "Color"];
+var defaultTags = ["abstract", "minimal", "geometric", "trippy", "illustration", "3d", "design", "motion graphics", "pattern", "color"];
 
 function renderTags(tagArr) {
   $("#tagContainer").empty();
   for (var i = 0; i < tagArr.length; i++) {
-    var newBtn = $('<button type="button" class="btn btn-primary btn-sm mt-2 mr-1 tag">');
+    var newBtn = $('<button type="button" class="btn btn-primary btn-sm mt-2 mr-1 tag text-capitalize">');
     newBtn.attr("data-tag", tagArr[i]);
     newBtn.text(tagArr[i]);
     $("#tagContainer").append(newBtn);
@@ -24,20 +24,22 @@ function fetchGifs(tag) {
     method: "GET"
   }).then(function(response){
     $("#gifContainer").empty();
+    console.log(response);
     for (var i = 0; i < response.data.length; i++) {
       var stillUrl = response.data[i].images["480w_still"].url;
       var gifUrl = response.data[i].images.downsized.url;
-      var newGif = $("<img class='gif'>").attr("data-gifurl", gifUrl).attr("data-stillurl", stillUrl).attr("data-imgtype", "still").attr("src", stillUrl);
-      var overlay = $("<div class='gifOverlay'>");
-      var gifDiv = $("<div class='gifDiv'>").append(newGif, overlay);
+      var newGif = $("<img class='gif' style='display:none;'>").attr("data-gifurl", gifUrl).attr("data-stillurl", stillUrl).attr("data-imgtype", "still").attr("src", stillUrl);
+      newGif.bind("load", function () { $(this).fadeIn(); });
+      //var overlay = $("<div class='gifOverlay'>");
+      var downloadBtn = $("<a class='btn btn-secondary btn-sm download' target='_blank' download>").text("Download").attr("href", gifUrl);
+      var gifDiv = $("<div class='gifDiv'>").append(newGif, downloadBtn);
       $("#gifContainer").append(gifDiv);
     }
   })
 }
 
 function addUserTag() {
-  var userTag = $("#addTagInput").val().trim();
-  console.log(userTag);
+  var userTag = $("#addTagInput").val().trim().toLowerCase();
   if (userTag.length > 2 && defaultTags.indexOf(userTag) === -1) {
     defaultTags.push(userTag);
     renderTags(defaultTags);
@@ -78,10 +80,17 @@ $("#addTag").on("click", function(){
   addUserTag();
 });
 
-$("#gifContainer").on("click", ".gifOverlay", function(){
-  var thisGif = $(this).siblings(".gif");
-  console.log(thisGif);
+$("#gifContainer").on("click", ".gif", function(){
+  var thisGif = $(this);
   toggleGif(thisGif);
+});
+
+//toggle download button
+$("#gifContainer").on("mouseenter", ".gifDiv", function(){
+  $(this).children(".download").show();
+});
+$("#gifContainer").on("mouseleave", ".gifDiv", function(){
+  $(this).children(".download").hide();
 });
 
 });
